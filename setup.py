604113dfeaf13d278f8583b308fbd25554dd7313
farah mhedhbi <mhedhbyfarah@gmail.com>
2026-05-11 23:30:45 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="LLM-medical-chatbot",
+    version="0.1",
+    author="MHEDHBI Farah",
+    author_email="mhedhbyfarah@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
